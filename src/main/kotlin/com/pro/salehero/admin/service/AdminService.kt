d52dc7ee4701d00c78fc00c37b45a434e24a04ce
chhong <pnci1029@gmail.com>
2025-04-19 12:00:30 +0900

    컨텐트 조정

--- src/main/kotlin/com/pro/salehero/admin/service/AdminService.kt
+++ src/main/kotlin/com/pro/salehero/admin/service/AdminService.kt
@@ -1,5 +1,6 @@
 package com.pro.salehero.admin.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.pro.salehero.newsletter.controller.dto.NewsLetterPostDTO
 import com.pro.salehero.newsletter.domain.NewsLetter
 import com.pro.salehero.newsletter.domain.NewsLetterRepository
@@ -7,14 +8,15 @@ import org.springframework.stereotype.Service
 
 @Service
 class AdminService(
-    private val newsLetterRepository: NewsLetterRepository
+    private val newsLetterRepository: NewsLetterRepository,
+    private val objectMapper: ObjectMapper
 ){
     fun createNewsLetter(
         dto: NewsLetterPostDTO
     ) = apply { newsLetterRepository.save(
         NewsLetter(
             title = dto.title,
-            content = dto.content,
+            content = objectMapper.writeValueAsString(dto.content),
             isSent = "N",
             sentAt = dto.sentAt
         )
