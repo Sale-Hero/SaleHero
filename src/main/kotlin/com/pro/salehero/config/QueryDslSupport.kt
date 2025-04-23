cc1192088e22a6da9077bfcab9858b9717b3b5b5
chhong <pnci1029@gmail.com>
2025-04-23 19:04:09 +0900

    페이징 공통 수정

--- src/main/kotlin/com/pro/salehero/config/QueryDslSupport.kt
+++ src/main/kotlin/com/pro/salehero/config/QueryDslSupport.kt
@@ -3,6 +3,7 @@ package com.pro.salehero.config
 import com.pro.salehero.common.PageResponseDTO
 import com.querydsl.jpa.impl.JPAQuery
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 
 abstract class QueryDslSupport(
@@ -13,20 +14,32 @@ abstract class QueryDslSupport(
         countQuery: JPAQuery<Long>,
         pageable: Pageable
     ): PageResponseDTO<T> {
+        val page = clientPageConverter(pageable)
+
         val paginatedQuery = contentQuery
-            .offset(pageable.offset)
-            .limit(pageable.pageSize.toLong())
+            .offset(page.offset)
+            .limit(page.pageSize.toLong())
 
         // 결과 조회
         val content = paginatedQuery.fetch()
         val total = countQuery.fetchOne() ?: 0L
         val totalPages = if (total == 0L) 0 else
-            (total / pageable.pageSize + if (total % pageable.pageSize > 0) 1 else 0).toInt()
+            (total / page.pageSize + if (total % page.pageSize > 0) 1 else 0).toInt()
 
         return PageResponseDTO(
             totalPages = totalPages,
             totalElement = total,
             content = content
+        )
+    }
+
+    private fun clientPageConverter(
+        pageable: Pageable
+    ): Pageable {
+        return PageRequest.of(
+            pageable.pageNumber - 1,
+            pageable.pageSize,
+            pageable.sort
         )
     }
 }
