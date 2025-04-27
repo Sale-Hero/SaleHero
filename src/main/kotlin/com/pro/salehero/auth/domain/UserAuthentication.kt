e8c9d103726fd819a6fff299185314ea1a9d3f78
chhong <pnci1029@gmail.com>
2025-04-27 19:40:00 +0900

    메일 인증 API

---
+++ src/main/kotlin/com/pro/salehero/auth/domain/UserAuthentication.kt
@@ -0,0 +1,16 @@
+package com.pro.salehero.auth.domain
+
+import com.pro.salehero.util.CreateAndUpdateAudit
+import jakarta.persistence.*
+
+@Entity
+data class UserAuthentication(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    val email: String,
+    @Column(nullable = false, length = 8)
+    var code: String,
+) : CreateAndUpdateAudit()
