4e223a6e03308ae259a33898f19cf295bd7684a7
chhong <pnci1029@gmail.com>
2025-04-27 19:39:52 +0900

    메일 인증 API

---
+++ src/main/kotlin/com/pro/salehero/util/CreateAudit.kt
@@ -0,0 +1,17 @@
+package com.pro.salehero.util
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class CreateAudit(
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+)
