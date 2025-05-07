5b59e2ee02301f95e0de3dc6468bb7c169afe307
chhong <pnci1029@gmail.com>
2025-05-07 18:51:05 +0900

    뉴스레터 유저용 관리자용 분리중

---
+++ src/main/kotlin/com/pro/salehero/newsletter/service/AdminNewsLetterService.kt
@@ -0,0 +1,71 @@
+package com.pro.salehero.newsletter.service
+
+import com.pro.salehero.common.dto.ResponseDTO
+import com.pro.salehero.newsletter.controller.dto.NewsLetterDeleteDTO
+import com.pro.salehero.newsletter.controller.dto.NewsLetterPostDTO
+import com.pro.salehero.newsletter.controller.dto.NewsLetterPutDTO
+import com.pro.salehero.newsletter.domain.NewsLetter
+import com.pro.salehero.newsletter.domain.NewsLetterRepository
+import com.pro.salehero.util.exception.CustomException
+import com.pro.salehero.util.exception.ErrorCode
+import com.pro.salehero.util.security.SecurityUtil.Companion.getCurrentUser
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class AdminNewsLetterService(
+    private val newsLetterRepository: NewsLetterRepository
+) {
+    @Transactional
+    fun createNewsLetter(
+        dto: NewsLetterPostDTO
+    ) = getCurrentUser()
+        .let {
+            NewsLetter(
+                title = dto.title,
+                content = dto.content,
+                isSent = "N",
+                isPublic = "N",
+                sentAt = dto.sentAt
+            )
+        }
+        .let { newsLetterRepository.save(it) }
+        .also { ResponseEntity.ok() }
+
+    @Transactional(readOnly = true)
+    fun getNewsLetters(
+        pageable: Pageable,
+        query: String?,
+    ) = newsLetterRepository.searchAdminNewsLetter(pageable, query)
+
+    @Transactional
+    fun updateNewsLetter(
+        dto: NewsLetterPutDTO
+    ) = getCurrentUser()
+        .let { newsLetterRepository.updateNewsLetter(dto) }
+        .let {
+            if (it >= 1) {
+                ResponseEntity.ok().body(it)
+            } else {
+                throw CustomException(ErrorCode.CODE_404)
+            }
+        }
+
+    @Transactional
+    fun removeNewsLetters(dto: NewsLetterDeleteDTO): ResponseDTO<Long> {
+        var deletedCount = 0L
+
+        dto.idxList.forEach { idx ->
+            val result = newsLetterRepository.remove(idx)
+            deletedCount += result
+        }
+
+        return ResponseDTO(
+            success = true,
+            message = "뉴스레터 삭제가 완료되었습니다.",
+            data = deletedCount
+        )
+    }
+}
