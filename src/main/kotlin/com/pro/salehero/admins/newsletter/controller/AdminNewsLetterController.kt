abf68cce22bb65d5d62eb2c4c4524ae453054591
chhong <pnci1029@gmail.com>
2025-05-15 17:27:34 +0900

    패키지 정리중

---
+++ src/main/kotlin/com/pro/salehero/admins/newsletter/controller/AdminNewsLetterController.kt
@@ -0,0 +1,55 @@
+package com.pro.salehero.newsletter.controller
+
+import com.pro.salehero.users.newsletter.controller.dto.NewsLetterDeleteDTO
+import com.pro.salehero.users.newsletter.controller.dto.NewsLetterPostDTO
+import com.pro.salehero.users.newsletter.controller.dto.NewsLetterPutDTO
+import com.pro.salehero.users.newsletter.service.AdminNewsLetterService
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/admin/news")
+class AdminNewsLetterController (
+    private val adminNewsLetterService: AdminNewsLetterService
+){
+    @PostMapping
+    fun createNewsLetter(
+        @RequestBody dto: NewsLetterPostDTO
+    ) = adminNewsLetterService.createNewsLetter(dto)
+
+    @GetMapping
+    fun getNewsLetters(
+        @RequestParam(required = false) query: String?,
+        @PageableDefault(
+            sort = ["createAt"],
+            direction = Sort.Direction.DESC,
+            size = 7,
+            page = 1
+        ) pageable: Pageable,
+    ) = adminNewsLetterService.getNewsLetters(pageable, query)
+
+//    @GetMapping("/{id}")
+//    fun getNewsLetter(
+//        @PathVariable("id") id: Long
+//    ) = adminNewsLetterService.getNewsLetter(id)
+
+    @PutMapping("/{idx}")
+    fun updateNewsLetter(
+        @RequestBody dto: NewsLetterPutDTO,
+        @PathVariable("idx") idx: Long
+    ) = adminNewsLetterService.updateNewsLetter(
+        NewsLetterPutDTO(
+            idx = idx,
+            title = dto.title,
+            content = dto.content,
+            sentAt = dto.sentAt,
+        )
+    )
+
+    @DeleteMapping
+    fun removeNewsLetter(
+        @RequestBody dto: NewsLetterDeleteDTO,
+    ) = adminNewsLetterService.removeNewsLetters(dto)
+}
