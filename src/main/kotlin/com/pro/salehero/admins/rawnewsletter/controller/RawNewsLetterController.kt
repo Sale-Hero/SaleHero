a2fb73fcc58d299c6f5a4c7f113202153d934a4c
chhong <pnci1029@gmail.com>
2025-05-15 19:51:30 +0900

    등록 추가

--- src/main/kotlin/com/pro/salehero/admins/rawnewsletter/controller/RawNewsLetterController.kt
+++ src/main/kotlin/com/pro/salehero/admins/rawnewsletter/controller/RawNewsLetterController.kt
@@ -1,6 +1,9 @@
 package com.pro.salehero.admins.rawnewsletter.controller
 
+import com.pro.salehero.admins.newsletter.controller.dto.RawNewsLetterPostDTO
 import com.pro.salehero.admins.rawnewsletter.service.RawNewsLetterService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 class RawNewsLetterController (
     private val rawNewsLetterService: RawNewsLetterService
 ){
+
+    @PostMapping
+    fun generate(
+        @RequestBody rawNewsLetterPostDTO: RawNewsLetterPostDTO
+    ) = rawNewsLetterService.generateRawNewsLetter(rawNewsLetterPostDTO)
 }
--- src/main/kotlin/com/pro/salehero/admins/rawnewsletter/service/RawNewsLetterService.kt
+++ src/main/kotlin/com/pro/salehero/admins/rawnewsletter/service/RawNewsLetterService.kt
@@ -1,11 +1,22 @@
 package com.pro.salehero.admins.rawnewsletter.service
 
+import com.pro.salehero.admins.newsletter.controller.dto.RawNewsLetterPostDTO
+import com.pro.salehero.admins.rawnewsletter.domain.RawNewsLetter
 import com.pro.salehero.admins.rawnewsletter.domain.RawNewsLetterRepository
+import com.pro.salehero.common.dto.ResponseDTO
 import org.springframework.stereotype.Service
 
 @Service
 class RawNewsLetterService(
     private val rawNewsLetterRepository: RawNewsLetterRepository
 ) {
+    fun generateRawNewsLetter(
+        rawNewsLetterPostDTO: RawNewsLetterPostDTO
+    ): ResponseDTO<Boolean> = RawNewsLetter(
+        title = rawNewsLetterPostDTO.title,
+        content = rawNewsLetterPostDTO.content,
+    )
+        .also { rawNewsLetterRepository.save(it) }
+        .let { ResponseDTO(false, "데이터 추가 완료", false) }
 
 }
