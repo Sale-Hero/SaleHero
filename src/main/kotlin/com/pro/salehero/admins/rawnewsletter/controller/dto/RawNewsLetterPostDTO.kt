4b1ef4c6f3749389dbc9a0ce1947f2c2e191b4ba
chhong <pnci1029@gmail.com>
2025-05-17 12:23:19 +0900

    dto 수정

--- src/main/kotlin/com/pro/salehero/admins/rawnewsletter/controller/dto/RawNewsLetterPostDTO.kt
+++ src/main/kotlin/com/pro/salehero/admins/rawnewsletter/controller/dto/RawNewsLetterPostDTO.kt
@@ -1,9 +1,10 @@
-package com.pro.salehero.admins.newsletter.controller.dto
+package com.pro.salehero.admins.rawnewsletter.controller.dto
 
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 data class RawNewsLetterPostDTO(
+    val id: Long?,
     @field:NotBlank
     @field:NotNull
     val title: String,
