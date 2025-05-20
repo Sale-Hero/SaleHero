08c4a780f801e07dab2471c89f7108ea2c51c32f
chhong <pnci1029@gmail.com>
2025-05-20 18:35:06 +0900

    테스트 센더 API 완성

--- src/main/kotlin/com/pro/salehero/admins/newsletter/service/AdminNewsLetterService.kt
+++ src/main/kotlin/com/pro/salehero/admins/newsletter/service/AdminNewsLetterService.kt
@@ -2,6 +2,7 @@ package com.pro.salehero.newsletter.service
 
 import com.pro.salehero.admins.newsletter.controller.dto.SendTestMailDTO
 import com.pro.salehero.common.dto.ResponseDTO
+import com.pro.salehero.common.service.MailSenderService
 import com.pro.salehero.users.newsletter.controller.dto.NewsLetterDeleteDTO
 import com.pro.salehero.users.newsletter.controller.dto.NewsLetterPostDTO
 import com.pro.salehero.users.newsletter.controller.dto.NewsLetterPutDTO
@@ -10,14 +11,18 @@ import com.pro.salehero.users.newsletter.domain.NewsLetterRepository
 import com.pro.salehero.util.exception.CustomException
 import com.pro.salehero.util.exception.ErrorCode
 import com.pro.salehero.util.security.SecurityUtil.Companion.getCurrentUser
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import javax.naming.Context
 
 @Service
 class AdminNewsLetterService(
-    private val newsLetterRepository: NewsLetterRepository
+    private val newsLetterRepository: NewsLetterRepository,
+    private val mailSenderService: MailSenderService,
+    @Value("\${smtp.email}") private val sender: String,
 ) {
     @Transactional
     fun createNewsLetter(
@@ -70,9 +75,25 @@ class AdminNewsLetterService(
         )
     }
 
+    @Transactional
     fun sendMockMail(
         dto: SendTestMailDTO
-    ) =  {
-        println("hello world")
+    ) {
+        val newsLetter = newsLetterRepository.findById(dto.id)
+            .orElseThrow { CustomException(ErrorCode.CODE_404) }
+
+        val context = org.thymeleaf.context.Context()
+        context.setVariable("dealTitle", newsLetter.title)
+        context.setVariable("dealId", newsLetter.id)
+        context.setVariable("email", dto.target)
+
+        mailSenderService.sendEmail(
+            to = dto.target ?:"pnci1029@gmail.com",
+            subject = "테스트용 메일입니다.",
+            templateName = "saleInformation",
+            context = context,
+            mailSender = sender
+        )
+        println("끝")
     }
 }
