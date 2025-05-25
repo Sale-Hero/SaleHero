cb5a3b21378b180db42bddb65f036acd09da22f6
chhong <pnci1029@gmail.com>
2025-05-25 19:12:14 +0900

    상세 게시글 API 추가 트랜잭셔널 서비스 전체

--- src/main/kotlin/com/pro/salehero/users/community/service/CommunityService.kt
+++ src/main/kotlin/com/pro/salehero/users/community/service/CommunityService.kt
@@ -1,17 +1,27 @@
 package com.pro.salehero.users.community.service
 
+import com.pro.salehero.common.enums.RedisContentType
+import com.pro.salehero.common.service.ViewCountService
+import com.pro.salehero.common.service.dto.ViewCount
 import com.pro.salehero.users.community.controller.dto.CommunityPostDTO
 import com.pro.salehero.users.community.controller.dto.CommunityResponseDTO
 import com.pro.salehero.users.community.controller.dto.CommunitySearchDTO
 import com.pro.salehero.users.community.domain.Community
 import com.pro.salehero.users.community.domain.CommunityRepository
+import com.pro.salehero.util.comfortutil.ComfortUtil
+import com.pro.salehero.util.exception.CustomException
+import com.pro.salehero.util.exception.ErrorCode
 import com.pro.salehero.util.security.SecurityUtil.Companion.getCurrentUser
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
-@Service
+@Service @Transactional
 class CommunityService(
     private val communityRepository: CommunityRepository,
+    private val viewCountService: ViewCountService,
+    private val comfortUtil: ComfortUtil,
 ) {
     fun createArticle(
         communityPostDTO: CommunityPostDTO
@@ -39,10 +49,29 @@ class CommunityService(
         )
     }
 
+    @Transactional(readOnly = true)
     fun getArticles(
         dto: CommunitySearchDTO,
         pageable: Pageable
     ) = communityRepository.getArticles(dto, pageable)
 
+    @Transactional(readOnly = true)
+    fun getArticle(
+        id: Long,
+        request: HttpServletRequest
+    ):CommunityResponseDTO  {
+        val article = communityRepository.findById(id)
+        if (article.isEmpty) {
+            throw CustomException(ErrorCode.CODE_404)
+        }
+        if (article.get().isDeleted == "Y") {
+            throw CustomException(ErrorCode.CODE_4042)
+        }
+        viewCountService.increaseViewCount(RedisContentType.COMMUNITY, article.get().id!!, comfortUtil.getUserIdentifier(request))
+
+        return CommunityResponseDTO.of(article.get())
+    }
+
+    fun updateViewCount(viewCount: ViewCount) = communityRepository.updateViewCount(viewCount)
 
 }
