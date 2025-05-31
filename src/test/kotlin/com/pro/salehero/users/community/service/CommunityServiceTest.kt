bad48aa2ea3899345a03aeeea20ea05a470bbd89
chhong <pnci1029@gmail.com>
2025-05-31 18:11:09 +0900

    커뮤니티 서비스 로직 분리 테스트 수정

--- src/test/kotlin/com/pro/salehero/users/community/service/CommunityServiceTest.kt
+++ src/test/kotlin/com/pro/salehero/users/community/service/CommunityServiceTest.kt
@@ -1,25 +1,11 @@
 package com.pro.salehero.users.community.service
 
-import com.pro.salehero.common.service.ViewCountService
 import com.pro.salehero.users.community.controller.dto.CommunityPostDTO
-import com.pro.salehero.users.community.domain.Community
-import com.pro.salehero.users.community.domain.CommunityRepository
 import com.pro.salehero.users.community.domain.enums.CommunityCategory
 import com.pro.salehero.users.user.domain.User
 import com.pro.salehero.users.user.domain.UserRepository
 import com.pro.salehero.users.user.domain.enums.UserRole
-import com.pro.salehero.users.user.service.UserService
-import com.pro.salehero.util.comfortutil.ComfortUtil
-import com.pro.salehero.util.security.SecurityUtil.Companion.getCurrentUser
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.ArgumentMatchers.any
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when`
-import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.context.support.WithMockUser
@@ -39,10 +25,7 @@ class CommunityServiceTest {
     @WithMockUser(username = "test@test.com")
     fun `createArticle - 성공`() {
         // given
-        val user = User(
-            1L, "test@test.com", "test", "test","Y", UserRole.USER
-        )
-        userRepository.save(user)
+        val user = User(1L, "test@test.com", "test", "test","Y", UserRole.USER)
 
         val communityPostDTO = CommunityPostDTO(
             title = "테스트 제목",
@@ -51,7 +34,7 @@ class CommunityServiceTest {
         )
 
         // when
-        val result = communityService.createArticle(communityPostDTO)
+        val result = communityService.createArticleWithUser(user, communityPostDTO)
 
         // then
         assertThat(result.title).isEqualTo("테스트 제목")
