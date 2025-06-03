41f05cf08b92dbb0381534c348fb227af7d38aaa
chhong <pnci1029@gmail.com>
2025-06-03 13:33:01 +0900

    컨트롤러 생성 테스트 추가

--- src/test/kotlin/com/pro/salehero/users/community/controller/CommunityControllerTest.kt
+++ src/test/kotlin/com/pro/salehero/users/community/controller/CommunityControllerTest.kt
@@ -2,20 +2,30 @@ package com.pro.salehero.users.community.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.pro.salehero.users.community.controller.dto.CommunityPostDTO
+import com.pro.salehero.users.community.controller.dto.CommunityResponseDTO
 import com.pro.salehero.users.community.domain.enums.CommunityCategory
 import com.pro.salehero.users.community.service.CommunityService
+import com.pro.salehero.users.user.domain.User
+import com.pro.salehero.users.user.domain.enums.UserRole
+import com.pro.salehero.util.security.SecurityUtil
+import com.pro.salehero.util.security.SecurityUtil.Companion.getCurrentUser
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.LocalDateTime
 import kotlin.test.Test
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = [CommunityController::class])
+@AutoConfigureMockMvc(addFilters = false)
 class CommunityControllerTest {
 
     @MockBean
@@ -27,23 +37,37 @@ class CommunityControllerTest {
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
+
     @Test
     fun `postArticle - 게시글 작성 성공`() {
         // given
         val communityDTO = createCommunityDTO("제목1", "내용 1")
+        val expectedResult = CommunityResponseDTO(
+            id = 1L,
+            title = "제목1",
+            content ="내용 1",
+            createdAt = LocalDateTime.now(),
+            viewCount = 0L,
+            writerName = "허허"
+        )
 
+        given(communityService.createArticle(communityDTO)).willReturn(expectedResult)
+
+        // when // then
         mockMvc.perform(
             post("/api/community")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(communityDTO))
+                .with(csrf())
         )
             .andExpect(status().isOk)
+            .andExpect(jsonPath("$.title").value("제목1"))
+            .andExpect(jsonPath("$.content").value("내용 1"))
 
-
-        // when
-
-        // then
+        verify(communityService).createArticle(communityDTO)
     }
+
+
 
     private fun createCommunityDTO(
         title: String,
