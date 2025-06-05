858b2b8cc50e903e37931b817a2c2bd70deb23f8
chhong <pnci1029@gmail.com>
2025-06-05 20:35:02 +0900

    세팅중

---
+++ src/main/kotlin/com/pro/salehero/common/service/KakaoClientService.kt
@@ -0,0 +1,14 @@
+package com.pro.salehero.common.service
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class KakaoClientService (
+
+){
+    private val logger = LoggerFactory.getLogger(KakaoClientService::class.java)
+    fun getToken() {
+
+    }
+}
