2843556fbb7aeb047b9931e5132ba35bf53b1415
chhong <pnci1029@gmail.com>
2025-06-05 20:50:55 +0900

    토큰 발급

--- src/main/kotlin/com/pro/salehero/common/service/kakao/KakaoApiClientImpl.kt
+++ src/main/kotlin/com/pro/salehero/common/service/kakao/KakaoApiClientImpl.kt
@@ -1,12 +1,43 @@
 package com.pro.salehero.common.service.kakao
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+import java.util.*
 
+@Component
 class KakaoApiClientImpl(
     @Value("\${kakao.clientId}") private val clientId: String,
     @Value("\${kakao.clientSecret}") private val clientSecret: String,
 ): KakaoApiClient {
+
+    companion object {
+        private const val TOKEN_BASE_URL = "https://bizmsg-web.kakaoenterprise.com/v2/oauth/token"
+
+
+    }
+
+    private val restTemplate = RestTemplate()
+
     override fun getToken(): String {
-        TODO("Not yet implemented")
+        val headers = HttpHeaders().apply {
+            accept = listOf(MediaType.parseMediaType("*/*"))
+            contentType = MediaType.APPLICATION_FORM_URLENCODED
+            set("Authorization", "Basic ${Base64.getEncoder().encodeToString("$clientId:$clientSecret".toByteArray())}")
+        }
+
+        val body: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>().apply {
+            add("grant_type", "client_credentials")
+        }
+
+        val request = HttpEntity(body, headers)
+        val response = restTemplate.postForEntity(TOKEN_BASE_URL, request, String::class.java)
+
+        return response.body ?: throw RuntimeException("토큰 요청 실패")
     }
 }
--- src/main/kotlin/com/pro/salehero/users/user/controller/TestController.kt
+++ src/main/kotlin/com/pro/salehero/users/user/controller/TestController.kt
@@ -1,16 +1,20 @@
 package com.pro.salehero.users.user.controller
 
+import com.pro.salehero.common.service.kakao.KakaoApiClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestController {
+class TestController(
+    private val kakaoApiClient: KakaoApiClient
+) {
     @GetMapping("/")
     fun test(): String = "Hello World1"
 
     @GetMapping("/kakao-test")
-    fun kakaoTest() {
-
+    fun kakaoTest(): String {
+        val result =kakaoApiClient.getToken()
+        return result
     }
 
 }
