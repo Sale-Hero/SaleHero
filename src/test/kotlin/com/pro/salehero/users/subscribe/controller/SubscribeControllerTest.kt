37633dae9fea037fc16bdab18b1669cf3b500db8
chhong <pnci1029@gmail.com>
2025-07-05 22:56:23 +0900

    구독 컨트롤러 테스트

---
+++ src/test/kotlin/com/pro/salehero/users/subscribe/controller/SubscribeControllerTest.kt
@@ -0,0 +1,10 @@
+package com.pro.salehero.users.subscribe.controller
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class SubscribeControllerTest {
+
+
+}
