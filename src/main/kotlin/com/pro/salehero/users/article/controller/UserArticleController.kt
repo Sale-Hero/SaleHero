f73d1adbfdbd7d91ff778a0b531097092f1f892c
chhong <pnci1029@gmail.com>
2025-07-10 21:22:06 +0900

    셋업

--- src/main/kotlin/com/pro/salehero/users/article/controller/UserArticleController.kt
+++ src/main/kotlin/com/pro/salehero/users/article/controller/UserArticleController.kt
@@ -1,11 +1,12 @@
 package com.pro.salehero.users.article.controller
 
+import com.pro.salehero.users.article.service.UserArticleService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/articles")
 class UserArticleController (
-
+    private val userArticleService: UserArticleService,
 ){
 }
--- src/main/kotlin/com/pro/salehero/users/article/service/UserArticleService.kt
+++ src/main/kotlin/com/pro/salehero/users/article/service/UserArticleService.kt
@@ -1,9 +1,10 @@
 package com.pro.salehero.users.article.service
 
+import com.pro.salehero.users.article.domain.ArticleRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UserArticleService (
-
+    private val userArticleRepository: ArticleRepository
 ){
 }
