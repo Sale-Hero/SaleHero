41feb74c16060d658ad31bd72aaab45f9e20901a
chhong <pnci1029@gmail.com>
2025-07-10 21:04:30 +0900

    사용자용 아티클 API

---
+++ src/main/kotlin/com/pro/salehero/users/article/controller/UserArticleController.kt
@@ -0,0 +1,11 @@
+package com.pro.salehero.users.article.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/articles")
+class UserArticleController (
+
+){
+}
---
+++ src/main/kotlin/com/pro/salehero/users/article/service/UserArticleService.kt
@@ -0,0 +1,9 @@
+package com.pro.salehero.users.article.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserArticleService (
+
+){
+}
