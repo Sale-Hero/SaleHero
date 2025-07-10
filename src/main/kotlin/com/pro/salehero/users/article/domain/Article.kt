21a0f8624ce863f4c41fd28bf9ae45f2f816f255
chhong <pnci1029@gmail.com>
2025-07-10 21:04:24 +0900

    사용자용 아티클 엔티티

---
+++ src/main/kotlin/com/pro/salehero/users/article/domain/Article.kt
@@ -0,0 +1,25 @@
+package com.pro.salehero.users.article.domain
+
+import com.pro.salehero.users.community.domain.enums.ArticleCategory
+import com.pro.salehero.util.CreateAndUpdateAudit
+import jakarta.persistence.*
+
+@Entity
+class Article (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    val title: String,
+    @Lob // text
+    val content: String,
+    val summary: String,
+
+    @Enumerated(EnumType.STRING)
+    val category: ArticleCategory,
+    @Column(nullable = true, length = 1)
+    val isVisible: String = "N",
+    @Column(nullable = true, length = 1)
+    val isDeleted: String = "N",
+    val viewCount: Long,
+) : CreateAndUpdateAudit()
---
+++ src/main/kotlin/com/pro/salehero/users/article/domain/ArticleRepository.kt
@@ -0,0 +1,8 @@
+package com.pro.salehero.users.article.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ArticleRepository: JpaRepository<Article, Long> {
+}
