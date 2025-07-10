a32df839c58395e0c66c97681ee0237c908fd2a5
chhong <pnci1029@gmail.com>
2025-07-10 22:28:38 +0900

    밸리데이션 핸들링 추가

--- src/main/kotlin/com/pro/salehero/admins/article/controller/dto/AdminArticlePostDTO.kt
+++ src/main/kotlin/com/pro/salehero/admins/article/controller/dto/AdminArticlePostDTO.kt
@@ -1,10 +1,17 @@
 package com.pro.salehero.admins.article.controller.dto
 
 import com.pro.salehero.users.community.domain.enums.ArticleCategory
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class AdminArticlePostDTO (
+    @field:NotBlank
     val title: String,
+
+    @field:NotBlank
     val content: String,
     val summary: String,
+
+    @field:NotNull
     val category: ArticleCategory,
 )
