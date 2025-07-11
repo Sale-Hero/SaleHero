b8be9b47102d7cb5e4a70b25c85189c942cfcf45
chhong <pnci1029@gmail.com>
2025-07-11 19:31:51 +0900

    전체 조회 쿼리 추가

--- src/main/kotlin/com/pro/salehero/users/article/domain/ArticleRepositoryCustom.kt
+++ src/main/kotlin/com/pro/salehero/users/article/domain/ArticleRepositoryCustom.kt
@@ -1,7 +1,9 @@
 package com.pro.salehero.users.article.domain
 
+import com.pro.salehero.admins.article.controller.dto.AdminArticleDTO
 import com.pro.salehero.common.dto.PageResponseDTO
+import org.springframework.data.domain.Pageable
 
 interface ArticleRepositoryCustom {
-    fun getArticles(): PageResponseDTO<Article>
+    fun getArticles(pageable: Pageable): PageResponseDTO<AdminArticleDTO>
 }
--- src/main/kotlin/com/pro/salehero/users/article/domain/ArticleRepositoryImpl.kt
+++ src/main/kotlin/com/pro/salehero/users/article/domain/ArticleRepositoryImpl.kt
@@ -1,18 +1,40 @@
 package com.pro.salehero.users.article.domain
 
+import com.pro.salehero.admins.article.controller.dto.AdminArticleDTO
 import com.pro.salehero.common.dto.PageResponseDTO
 import com.pro.salehero.config.QueryDslSupport
+import com.pro.salehero.users.article.domain.QArticle.article
+import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.Pageable
 
 class ArticleRepositoryImpl(
     queryFactory: JPAQueryFactory
 ) : QueryDslSupport(queryFactory), ArticleRepositoryCustom {
 
 
-    override fun getArticles(): PageResponseDTO<Article> {
-        queryFactory
-            .select()
-        TODO("Not yet implemented")
+    override fun getArticles(
+        pageable: Pageable
+    ): PageResponseDTO<AdminArticleDTO> {
+        return fetchPageResponse(
+            pageable,
+            queryFactory
+                .select(
+                    Projections.constructor(
+                        AdminArticleDTO::class.java,
+                        article.id,
+                        article.title,
+                        article.content,
+                        article.summary,
+                        article.category,
+                        article.viewCount,
+                        article.isVisible,
+                        article.isDeleted,
+                    )
+                )
+                .from(article)
+                .orderBy(article.createdAt.desc())
+        )
     }
 
 }
