473a46460c439a73bf452299d2a3cbba0b5c2f99
chhong <pnci1029@gmail.com>
2025-07-11 19:31:54 +0900

    전체 조회 쿼리 추가

---
+++ src/main/kotlin/com/pro/salehero/admins/article/controller/dto/AdminArticleDTO.kt
@@ -0,0 +1,14 @@
+package com.pro.salehero.admins.article.controller.dto
+
+import com.pro.salehero.users.community.domain.enums.ArticleCategory
+
+data class AdminArticleDTO (
+    val id: String,
+    val title: String,
+    val content: String,
+    val summary: String? = null,
+    val category: ArticleCategory,
+    val viewCount: Long,
+    val isVisible: String,
+    val isDeleted: String,
+)
