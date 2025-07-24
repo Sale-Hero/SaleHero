1d153d8af9cfc846168a741ffcd9dbfc0d055d62
chhong <pnci1029@gmail.com>
2025-07-24 16:39:29 +0900

    아티클 업데이트 스케줄링

--- src/main/kotlin/com/pro/salehero/common/scheduling/ViewCountScheduler.kt
+++ src/main/kotlin/com/pro/salehero/common/scheduling/ViewCountScheduler.kt
@@ -1,8 +1,11 @@
 package com.pro.salehero.common.scheduling
 
+import com.pro.salehero.admins.article.service.AdminArticleService
 import com.pro.salehero.common.enums.RedisContentType
 import com.pro.salehero.common.service.ViewCountService
+import com.pro.salehero.config.SecurityDebugFilter
 import com.pro.salehero.users.community.service.CommunityService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -10,11 +13,18 @@ import org.springframework.stereotype.Component
 class ViewCountScheduler(
     private val viewCountService: ViewCountService,
     private val communityService: CommunityService,
+    private val adminArticleService: AdminArticleService,
 ) {
-//    @Scheduled(cron = "0/10 * * * * *", zone = "Asia/Seoul")
+    private val log = LoggerFactory.getLogger(SecurityDebugFilter::class.java)
+
+    @Scheduled(cron = "1 * * * * *", zone = "Asia/Seoul")
     fun updateCommunityViewCount() {
          viewCountService.getAllViewCounts(RedisContentType.COMMUNITY)
             .forEach { communityService.updateViewCount(it) }
-             .also { println("업데이트 완료") }
+             .also { log.info("커뮤니티 업데이트 완료") }
+
+        viewCountService.getAllViewCounts(RedisContentType.ARTICLE)
+            .forEach{ adminArticleService.updateViewCount(it)}
+            .also { log.info("아티클 업데이트 완료") }
     }
 }
