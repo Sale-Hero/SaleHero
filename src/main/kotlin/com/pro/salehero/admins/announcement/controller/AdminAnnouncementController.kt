cdd0c6f81cec09b5b495f2c96f141e77ccb5bf30
chhong <pnci1029@gmail.com>
2025-07-26 17:28:49 +0900

    공지사항 추가 API

--- src/main/kotlin/com/pro/salehero/admins/announcement/controller/AdminAnnouncementController.kt
+++ src/main/kotlin/com/pro/salehero/admins/announcement/controller/AdminAnnouncementController.kt
@@ -3,10 +3,14 @@ package com.pro.salehero.admins.announcement.controller
 import com.pro.salehero.admins.announcement.service.AdminAnnouncementService
 import com.pro.salehero.common.dto.PageResponseDTO
 import com.pro.salehero.domain.announcement.dto.AdminAnnouncementDTO
+import com.pro.salehero.domain.announcement.dto.AnnouncementPostDTO
+import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController
 class AdminAnnouncementController(
     private val adminAnnouncementService: AdminAnnouncementService
 ) {
+    @PostMapping
+    fun createAnnouncement(
+        @RequestBody @Valid announcementDTO: AnnouncementPostDTO
+    ) = adminAnnouncementService.createAnnouncement(announcementDTO)
+
     @GetMapping
     fun getAdminAnnouncements(
         @PageableDefault(
--- src/main/kotlin/com/pro/salehero/admins/announcement/service/AdminAnnouncementService.kt
+++ src/main/kotlin/com/pro/salehero/admins/announcement/service/AdminAnnouncementService.kt
@@ -1,8 +1,11 @@
 package com.pro.salehero.admins.announcement.service
 
 import com.pro.salehero.common.dto.PageResponseDTO
+import com.pro.salehero.common.dto.ResponseDTO
 import com.pro.salehero.domain.announcement.AnnouncementRepository
 import com.pro.salehero.domain.announcement.dto.AdminAnnouncementDTO
+import com.pro.salehero.domain.announcement.dto.AnnouncementPostDTO
+import com.pro.salehero.domain.announcement.mapper.AnnouncementMapper
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,10 +14,24 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class AdminAnnouncementService (
     private val announcementRepository: AnnouncementRepository,
+    private val announcementMapper: AnnouncementMapper
 ){
+    fun createAnnouncement(
+        announcementDTO: AnnouncementPostDTO
+    ): ResponseDTO<AdminAnnouncementDTO> {
+        val announcement = announcementMapper.toEntity(announcementDTO)
+        val createdAnnouncement = announcementRepository.save(announcement)
+
+        return ResponseDTO(
+            success = true,
+            message = "등록 완료되었습니다.",
+            data = announcementMapper.toAdminDTO(createdAnnouncement)
+        )
+    }
 
     @Transactional(readOnly = true)
     fun getAdminAnnouncements(
         pageable: Pageable
     ): PageResponseDTO<AdminAnnouncementDTO> = announcementRepository.getAdminAnnouncements(pageable)
+
 }
