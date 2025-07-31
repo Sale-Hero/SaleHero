674b42819881f0ca8490f341175c6ef190d7591d
chhong <pnci1029@gmail.com>
2025-07-31 09:41:02 +0900

    수정 API 추가

--- src/main/kotlin/com/pro/salehero/admins/announcement/controller/AdminAnnouncementController.kt
+++ src/main/kotlin/com/pro/salehero/admins/announcement/controller/AdminAnnouncementController.kt
@@ -8,11 +8,7 @@ import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/admin/announcements")
@@ -33,4 +29,10 @@ class AdminAnnouncementController(
             page = 1
         ) pageable: Pageable,
     ): PageResponseDTO<AdminAnnouncementDTO> = adminAnnouncementService.getAdminAnnouncements(pageable)
+
+    @PutMapping("{articleId}")
+    fun updateAnnouncement(
+        @RequestBody @Valid announcementDTO: AnnouncementPostDTO,
+        @PathVariable("articleId") articleId: Long,
+    ) = adminAnnouncementService.updateAnnouncement(announcementDTO, articleId)
 }
--- src/main/kotlin/com/pro/salehero/admins/announcement/service/AdminAnnouncementService.kt
+++ src/main/kotlin/com/pro/salehero/admins/announcement/service/AdminAnnouncementService.kt
@@ -2,20 +2,23 @@ package com.pro.salehero.admins.announcement.service
 
 import com.pro.salehero.common.dto.PageResponseDTO
 import com.pro.salehero.common.dto.ResponseDTO
+import com.pro.salehero.domain.announcement.Announcement
 import com.pro.salehero.domain.announcement.AnnouncementRepository
 import com.pro.salehero.domain.announcement.dto.AdminAnnouncementDTO
 import com.pro.salehero.domain.announcement.dto.AnnouncementPostDTO
 import com.pro.salehero.domain.announcement.mapper.AnnouncementMapper
+import com.pro.salehero.util.exception.CustomException
+import com.pro.salehero.util.exception.ErrorCode
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
-class AdminAnnouncementService (
+class AdminAnnouncementService(
     private val announcementRepository: AnnouncementRepository,
     private val announcementMapper: AnnouncementMapper
-){
+) {
     fun createAnnouncement(
         announcementDTO: AnnouncementPostDTO
     ): ResponseDTO<AdminAnnouncementDTO> {
@@ -34,4 +37,23 @@ class AdminAnnouncementService (
         pageable: Pageable
     ): PageResponseDTO<AdminAnnouncementDTO> = announcementRepository.getAdminAnnouncements(pageable)
 
+    fun updateAnnouncement(
+        announcementDTO: AnnouncementPostDTO,
+        articleId: Long
+    ):ResponseDTO<AdminAnnouncementDTO> {
+        val announcement = isAnnouncementExist(articleId)
+
+        announcement.update(announcementDTO)
+
+        return ResponseDTO(
+            success = true,
+            message = "수정 완료되었습니다.",
+            data = announcementMapper.toAdminDTO(announcement)
+        )
+    }
+
+    private fun isAnnouncementExist(
+        articleId: Long,
+    ): Announcement = announcementRepository.findById(articleId)
+        .orElseThrow { CustomException(ErrorCode.CODE_404) }
 }
