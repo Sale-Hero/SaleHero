e4ddec4ad53ef00f0e88e5ba98fb3ebccb6130ed
chhong <pnci1029@gmail.com>
2025-10-29 21:46:50 +0900

    패키지 구조 변경중

--- src/main/kotlin/com/pro/salehero/common/service/dto/ViewCount.kt
+++
@@ -1,9 +0,0 @@
-package com.pro.salehero.common.service.dto
-
-import com.pro.salehero.common.enums.RedisContentType
-
-data class ViewCount(
-    val type: RedisContentType,
-    val id: Long,
-    val viewCount: Long
-)
--- src/main/kotlin/com/pro/salehero/users/subscribe/controller/dto/UnSubscribeDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.pro.salehero.users.subscribe.controller.dto
-
-data class UnSubscribeDTO(
-    val email: String,
-    val token: String
-)
