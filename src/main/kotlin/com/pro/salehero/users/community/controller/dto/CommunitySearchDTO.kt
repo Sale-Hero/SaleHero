7b833dde97264819fa6d4b626ea072dae96e7694
chhong <pnci1029@gmail.com>
2025-10-29 21:41:19 +0900

    패키지 구조 변경중

--- src/main/kotlin/com/pro/salehero/users/community/controller/dto/CommunitySearchDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.pro.salehero.users.community.controller.dto
-
-import com.pro.salehero.domain.community.enums.CommunityCategory
-
-
-data class CommunitySearchDTO(
-    val category: CommunityCategory?,
-)
