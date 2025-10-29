49ca224e5b1bd4be62836ed1174118ac5c1835f6
chhong <pnci1029@gmail.com>
2025-10-29 21:46:46 +0900

    패키지 구조 변경중

--- src/main/kotlin/com/pro/salehero/auth/controller/dto/TokenChangeDTO.kt
+++
@@ -1,5 +0,0 @@
-package com.pro.salehero.auth.controller.dto
-
-data class TokenChangeDTO(
-    val refreshToken: String,
-)
--- src/main/kotlin/com/pro/salehero/users/subscribe/controller/dto/SubscribePostDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.pro.salehero.users.subscribe.controller.dto
-
-data class SubscribePostDTO(
-    val userEmail: String,
-    val isMarketingAgreed: Boolean
-)
