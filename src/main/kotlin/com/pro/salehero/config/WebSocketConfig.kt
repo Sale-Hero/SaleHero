3d53bcc0f3c251bce5ac260bcd0d474063cd324f
chhong <pnci1029@gmail.com>
2025-11-13 21:17:04 +0900

    세션 id를 통한 채팅 연결 개선

--- src/main/kotlin/com/pro/salehero/config/WebSocketConfig.kt
+++ src/main/kotlin/com/pro/salehero/config/WebSocketConfig.kt
@@ -1,21 +1,47 @@
 package com.pro.salehero.config
 
+import com.pro.salehero.auth.security.WebSocketPrincipal
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.server.ServerHttpRequest
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler
+import java.security.Principal
+import java.util.*
 
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/api/ws-chat").setAllowedOriginPatterns("*").withSockJS()
+        registry.addEndpoint("/api/ws-chat")
+            .setHandshakeHandler(customHandshakeHandler())
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-        registry.enableSimpleBroker("/topic")
+        registry.enableSimpleBroker("/topic", "/queue")
         registry.setApplicationDestinationPrefixes("/app")
+        registry.setUserDestinationPrefix("/user")
+    }
+
+    @Bean
+    fun customHandshakeHandler(): DefaultHandshakeHandler {
+        return object : DefaultHandshakeHandler() {
+            override fun determineUser(
+                request: ServerHttpRequest,
+                wsHandler: WebSocketHandler,
+                attributes: MutableMap<String, Any>
+            ): Principal {
+                // Use the ID provided by the client in the 'login' header, or generate a new one if absent.
+                val userId = request.headers.getFirst("login") ?: UUID.randomUUID().toString()
+                return WebSocketPrincipal(userId)
+            }
+        }
     }
 }
